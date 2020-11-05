e832e2606a2883ea2e2438fb67c82f3c45c4320f
Patrick Hannan <patrick@hannan.com>
2020-11-05 00:26:11 -0800

    import all files to app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header/Header";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Projects from "./pages/Projects/Projects";
+import Home from "./pages/Home/Home";
+import Contact from "./pages/Contact/Contact";
+import "./assets/css/App.css";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
   );
 }
 
