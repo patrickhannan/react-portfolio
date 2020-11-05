863692059018622ff3ebecf57ac23a8bab2e5228
Patrick Hannan <patrick@hannan.com>
2020-11-05 15:06:11 -0800

    filled out project page

---
+++ src/components/Project/Project.jsx
@@ -0,0 +1,145 @@
+import React from "react";
+import "./Portfolio.css";
+import project1 from "./img/deployedapp.png";
+import budget from "./img/budget.png";
+import workschedule from "./img/workplanner.png";
+import weatherdashboard from "./img/dashboard.png"
+import fitnesstracker from "./img/fitness.png";
+import bandwich from "./img/workstation.png";
+
+const Portfolio = () => {
+  return (
+    <div className="container">
+      <main className="row">
+        <div className="col-md-8">
+          <article className="block">
+            <h1 className="block-header">Portfolio</h1>
+            <hr />
+            <div id="repo1" className="project row">
+              <div className="col-sm-12 col-md-12 col-lg-12">
+                <a
+                  href="https://patrickhannan.github.io/05-daily-planner/"
+                  target="_blank"
+                >
+                  <img
+                    className="img-fluid port-image"
+                    src={workschedule}
+                    alt="work day schedule"
+                  />
+                </a>
+                <a
+                  className="btn btn-dark text-center"
+                  href="Github Repo"
+                >
+                  GitHub
+                </a>
+              </div>
+            </div>
+            <div id="repo2" className="project row">
+              <div className="col-sm-12 col-md-12 col-lg-12">
+                <a
+                  href="https://patrickhannan.github.io/server-side-apis-HW/"
+                  target="_blank"
+                >
+                  <img
+                    className="img-fluid port-image"
+                    src={weatherdashboard}
+                    alt="weather dashboard"
+                  />
+                </a>
+                <a
+                  className="btn btn-dark text-center"
+                  href="https://github.com/patrickhannan/server-side-apis-HW"
+                >
+                  GitHub
+                </a>
+              </div>
+            </div>
+            <div id="repo3" className="project row">
+              <div className="col-sm-12 col-md-12 col-lg-12">
+                <a
+                  href="https://immense-bastion-96583.herokuapp.com/"
+                  target="_blank"
+                >
+                  <img
+                    className="img-fluid port-image"
+                    src={fitnesstracker}
+                    alt="fitness tracker"
+                  />
+                </a>
+                <a
+                  className="btn btn-dark text-center"
+                  href="https://github.com/patrickhannan/fitness-tracker"
+                >
+                  GitHub
+                </a>
+              </div>
+            </div>
+            <div id="repo4" className="project row">
+              <div className="col-sm-12 col-md-12 col-lg-12">
+                <a
+                  href="https://bandwich-app.herokuapp.com/"
+                  target="_blank"
+                >
+                  <img
+                    className="img-fluid port-image"
+                    src={bandwich}
+                    alt="bandwich"
+                  />
+                </a>
+                <a
+                  className="btn btn-dark text-center"
+                  href="https://github.com/patrickhannan/bandwich"
+                >
+                  GitHub
+                </a>
+              </div>
+            </div>
+            <div id="repo5" className="project row">
+              <div className="col-sm-12 col-md-12 col-lg-12">
+                <a
+                  href="https://kreid333.github.io/project-1/"
+                  target="_blank"
+                >
+                  <img
+                    className="img-fluid port-image"
+                    src={project1}
+                    alt="project1"
+                  />
+                </a>
+                <a
+                  className="btn btn-dark text-center"
+                  href="https://github.com/patrickhannan/project-1"
+                >
+                  GitHub
+                </a>
+              </div>
+            </div>
+            <div id="repo6" className="project row">
+              <div className="col-sm-12 col-md-12 col-lg-12">
+                <a
+                  href="https://safe-springs-30942.herokuapp.com/"
+                  target="_blank"
+                >
+                  <img
+                    className="img-fluid port-image"
+                    src={budget}
+                    alt="budget tracker"
+                  />
+                </a>
+                <a
+                  className="btn btn-dark text-center"
+                  href="https://github.com/patrickhannan/budget-tracker"
+                >
+                  GitHub
+                </a>
+              </div>
+            </div>
+          </article>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default Portfolio;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <div class="row">
+        <div class="col-sm-12">
+          <div class="card">
+            <div class="card-header">
+              <h2 class="text-center">About me</h2>
+            </div>
+            <div class="card-body">
+            <p>Hello, my name is Patrick Hannan. I am a 19 year old college student attending the University of Southern California, but I am taking a semester off to participate in the Georgia Tech Coding Bootcamp. I am majoring in Business Administration, but with the current pandemic I decided to take a semester leave of absense to learn something I wouldn't normally learn through my school's business program. As a result, I am brand new to coding. Originally, from Los Angeles, California, my family moved to Atlanta, Georgia when I was a kid so that is where I grew up. However, I am a massive LA sports fan. My favorite thing to do is to watch the Lakers or Dodgers games on television and in person. My other hobbies include outdoor activities golfing, playing baseball, kayaking, as well as photography. I love being outdoors, and these physical activities allow me to get exercise and hang out with friends. Photography, while not a physical activity, allows me to be creative and go out and see the world and its beauty.
+            </p>           
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Home;
